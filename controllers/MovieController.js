5bbca57099aea2266c4e19172dfb5841f77cd49a
lmadhuranga <lilan.maduranga@gmail.com>
2019-01-01 20:12:48 +0530

    Upgrade to es6 js

--- controllers/MovieController.js
+++ controllers/MovieController.js
@@ -1,15 +1,18 @@
 const MovieModel = require('../models/MovieModel');
-const MovieController = function (){
+class MovieController {
+  constructor() {
+    console.log('MovieController construcutr');
+  }
   
-  this.getAll = (req, res) => {
+  getAll(req, res) {
     MovieModel.find(function (err, movie) {
       if (err) return next(err);
       res.send(movie);
     })
   }
   
-  this.create = (req, res) => {
-    var movie = new MovieModel({
+  create(req, res) {
+    const movie = new MovieModel({
       name: req.body.name,
       path: req.body.path
     });
@@ -18,42 +21,30 @@ const MovieController = function (){
       if (err) {
         return next(err);
       }
-      res.send('Movie Created successfully');
+        res.send('Movie Created successfully');
     });
   }
   
-  this.get = (req, res) => {
+  get(req, res) {
     MovieModel.findById(req.params.id, (err, movie) => {
       if (err) return next(err);
       res.send(movie);
     });
   }
   
-  this.update = (req, res) => {
-    MovieModel.findByIdAndUpdate(
-      req.params.id,
-      { $set: req.body }, 
-      (err, movie) => {
-        if (err) return next(err);
-        res.send('Movie udpated.');
-      }
-    );
+  update(req, res) {
+    MovieModel.findByIdAndUpdate(req.params.id, { $set: req.body },(err, movie) => {
+      if (err) return next(err);
+      res.send('Movie udpated.');
+    });
   }
-
-  this.delete = (req, res) => {
+  
+  delete(req, res) {
     MovieModel.findByIdAndRemove(req.params.id, (err) => {
       if (err) return next(err);
       res.send('Deleted successfully!');
     });
-  }
-
-  return {
-    getAll:this.getAll,
-    create:this.create,
-    get:this.get,
-    update:this.update,
-    delete:this.delete
-  }
+  }  
 }
 
 module.exports = new MovieController();
